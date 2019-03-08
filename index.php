e01c77c1cc5d343a485c981cc0effc165ae36e7a
ashish maru <ashish@itechnotion.in>
2019-03-08 14:57:21 +0530

    master committ
    master committ

---
+++ index.php
@@ -0,0 +1,30 @@
+<?php
+
+        $url = "http://fcm.googleapis.com/fcm/send";
+        $token = "XXXXX";//mobile device token 
+        $serverKey="XXXX"; //fcm push serverkey
+    
+        $title = "title";//message title
+        $body = "body";//message body
+        $notification = array('title' =>$title , 'text' => $body, 'sound' => 'default', 'badge' => '1');
+        $arrayToSend = array('to' => $token, 'notification' => $notification,'priority'=>'high');
+        $json = json_encode($arrayToSend);
+        $headers = array();
+        $headers[] = 'Content-Type: application/json';
+        $headers[] = 'Authorization: key='. $serverKey;
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, $url);
+
+        curl_setopt($ch, CURLOPT_CUSTOMREQUEST,"POST");
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $json);
+        curl_setopt($ch, CURLOPT_HTTPHEADER,$headers);
+        //Send the request
+        $response = curl_exec($ch);
+        //Close request
+        if ($response === FALSE) {
+        die('FCM Send Error: ' . curl_error($ch));
+        }
+        curl_close($ch);
+
+
+?>
